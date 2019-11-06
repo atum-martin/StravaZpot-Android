f78eda447937619b779f52320c07526e8f73dead
atum-martin <martin.chapman4@mail.dcu.ie>
2019-11-06 15:50:55 +0000

    Fixed Strava API change for scope parameter in auth request.
    See documentation changes for scope param: http://developers.strava.com/docs/authentication/#detailsaboutrequestingaccess

--- lib/src/main/java/com/sweetzpot/stravazpot/authenticaton/api/AccessScope.java
+++ lib/src/main/java/com/sweetzpot/stravazpot/authenticaton/api/AccessScope.java
@@ -1,10 +1,15 @@
 package com.sweetzpot.stravazpot.authenticaton.api;
 
 public enum AccessScope {
-    PUBLIC("public"),
-    WRITE("write"),
-    VIEW_PRIVATE("view_private"),
-    VIEW_PRIVATE_WRITE("view_private,write");
+    PUBLIC("read_all"),
+    READ("activity:read"),
+    WRITE("activity:write"),
+    VIEW_PRIVATE("profile:read_all"),
+    VIEW_PRIVATE_WRITE("profile:read_all,activity:write"),
+    PROFILE_READ("profile:read_all"),    
+    PROFILE_WRITE("profile:write"),
+    FULL_WRITE("profile:write,activity:write"),
+    FULL_READ("profile:read_all,activity:read_all,read_all");
 
     private String rawValue;
 
